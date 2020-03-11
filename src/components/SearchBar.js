d1654ada3e099b0ef08208d926775cf382dde33a
Roman Onishchenko <orm81zp@gmail.com>
2020-03-11 22:23:33 +0200

    search bar scaffolding

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <Feather name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    alignItems: 'center',
+    borderRadius: 5,
+    flexDirection: 'row',
+    marginHorizontal: 15
+  }
+});
 
 export default SearchBar;
